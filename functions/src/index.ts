679a7e4fe18540cfae606a94f08b6899f94a051f
Jalen-G <goodloejalen@gmail.com>
2020-01-23 13:57:38 -0600

    added api (returns image url and price)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+//const cheerio = require('cheerio');
+//const cors = require('cors')({ origin: true });
+//const fetch = require('node-fetch');
+//const puppeteer = require('puppeteer');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  const stockxAPI = require('stockx-api');
+  const stockX = new stockxAPI();
+   
+  stockX.fetchProductDetails('https://stockx.com/adidas-yeezy-boost-700-magnet')
+  .then(product => response.send((parseResponse(product))));
+});
+
+function parseResponse(product) {
+  var shoeName = product.name;
+  shoeName = shoeName.replace(/\s+/g, '-');
+  return {
+    "img-url": "https://stockx-360.imgix.net/" + shoeName + "/Images/" + shoeName + "/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1568299976&w=1000",
+    "price": product.variants[14].market.averageDeadstockPrice
+  }
+}
